ad55e86f1b0481b0606fd1c92d377dce1cd3b926
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2024-10-05 16:58:42 +0100

    feat:Added support for React Query

---
+++ src/api.ts
@@ -0,0 +1,6 @@
+import { useQuery } from '@tanstack/react-query';
+
+
+const unsplashApi = process.env.UNSPLASH_API
+
+export const getSearchResults = () => {}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactQueryProvider from "./provider/ReactQueryProvider"
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReactQueryProvider>
+      <App />
+    </ReactQueryProvider>
+    
   </StrictMode>,
 )
